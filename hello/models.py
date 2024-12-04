4600fcfd8c823872e45bc996f8ce58ee01c54b36
MarjonaZa <nasimova12345678@gmail.com>
2024-12-04 09:51:44 +0700

    add class Comment for test

--- hello/models.py
+++ hello/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 #комиты
+
+class Comment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    text = models.TextField()
+    def __str__(self):
+        return self.name
